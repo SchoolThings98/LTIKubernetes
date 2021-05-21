2b90c49583f004891d104b498f287c77a5ff9f38
SchoolThings98 <2181854@my.ipleiria.pt>
2021-05-21 15:13:17 +0100

    BEGIN

---
+++ API.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LTIProject2
+{
+    class API
+    {
+        public void listNodes()
+        {
+
+        }
+        public void listNamespaces()
+        {
+
+        }
+        public void createNamespaces()
+        {
+
+        }
+        public void deleteNamespaces()
+        {
+
+        }
+        public void listPods()
+        {
+
+        }
+        public void createPods()
+        {
+
+        }
+        public void deletePods()
+        {
+
+        }
+        public void listDeployments()
+        {
+
+        }
+        public void createDeployments()
+        {
+
+        }
+        public void deleteDeployments()
+        {
+
+        }
+        public void listServices()
+        {
+
+        }
+        public void createServices()
+        {
+
+        }
+        public void deleteServices()
+        {
+
+        }
+    }
+}
